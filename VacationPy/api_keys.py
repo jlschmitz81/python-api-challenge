d4d8ab98247249854040f5558f4ab471d6e6af23
Jessica Schmitz <jessica.schmitz81@gmail.com>
2020-05-16 10:36:49 -0500

    final upload

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "588ab94daf3910cfa4baa43dbb5fcf04"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCTikQ8HeWA96yYdTFAmPFy4hSEizxA5x4"
